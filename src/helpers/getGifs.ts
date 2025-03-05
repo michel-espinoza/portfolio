44fa56f37840375386c0e647980a71ccc9dd4bb0
michel_espinoza <michel.dev26@gmail.com>
2025-03-05 14:51:37 -0300

    max gifs 12

--- src/helpers/getGifs.ts
+++ src/helpers/getGifs.ts
@@ -6,7 +6,7 @@ interface Gif {
 
 export const getGifs = async (category: string): Promise<Gif[]> => {
   const apiKey = "FUYrCtupQcyoR5eN9iQmoQIKoSiVWry4";
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=10&q=${encodeURIComponent(category)}`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=12&q=${encodeURIComponent(category)}`;
 
   const resp = await fetch(url);
   const { data } = await resp.json();
